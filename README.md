453b4cf0e73c6b3455c2f6259b2d537a967c0027
Nile <nilew@uw.edu>
2018-07-24 15:15:08 -0700

    Updated help and readme

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # StyleTransferBot
 Discord Bot to Perform Style Transfer on Custom Images
+
 *Last updated July 24, 2018*
 
-## How to run
+## How to Run (Server)
 To start this bot on a local server, do the following:
 1. Make sure you have Node.js installed
 2. Make sure you have installed Discord.js through `npm install discord.js`
@@ -13,6 +14,16 @@ To start this bot on a local server, do the following:
 7. Now you should be able to use the bot in your server. Enjoy!
 
 *Note*: We will make these instructions nicer and more clear later...
+
+## How to Use (in Discord)
+`@styletransferbot help` lists the commands for the bot.
+
+To use the bot, first mention the bot however it is called in your server (default @styletransfer) and then type a sentence describing what you would like. Please order your sentence such that the object/subject of interest comes first, and the style comes later. Both the object/subject and style should be in parenthesis.
+
+Examples:
+- `@styletransferbot please paint me some "maple leaves" in the style of "starry night"`
+- `@styletransferbot "cat" "gustav klimt"`
+- `@styletransferbot I would like "toast and eggs" to look like "yoshitaka amano"
 
 ## Terms of Use
 The result images of this bot should **not** be used for any commercial purposes. We have created this bot just for fun, not for profit or for other's profit.
--- commands/help.js
+++ commands/help.js
@@ -11,33 +11,26 @@ exports.run = (client, config, message, args) => {
       },
       title: "Style Transfer Bot Command Guide",
       url: "http://google.com",
-      description: "Chronus is a simple bot to help you keep your discord friends accountable for going to sleep on time...",
-      fields: [{
-        name: "!setzone @username [utc-time]",
-        value: "Set the timezone for a mentioned user. [utc-time] should be replaced with UTC time zones in the following format (change the number and sign to match your zone): **utc-0400** or **utc+0545**"
-      },
+      description: "The Delight Style Transfer Bot is a simple bot created to allow you to create fun, custom art style transferred images",
+      fields: [
       {
-        name: ".",
-        value: ".",
-      },
-      {
-        name: "@styletransfer please give me \"moutains\" in the style of \"starry night\"",
-        value: "Get the timezone for the mentioned user."
+        name: "@styletransferbot please give me \"moutains\" in the style of \"starry night\"",
+        value: "Mention this bot then type a sentence with (1) a subject (in quotes) and (2) a style (in quotes)."
       },
       {
         name: "..",
         value: "..",
       },
       {
-        name: "!gettime @username [hourFormat]",
-        value: "Get the current local time for the mentioned user. [hourFormat] is an optional argument and default to **12**. [hourFormat] should be either **12** (for am/pm format) or **24** (for 24 hour format)."
+        name: "@styletransferbot how much time is left?",
+        value: "Returns the estimated time remaining until your request is done"
       },
       {
         name: "...",
         value: "...",
       },
       {
-        name: "!help @Chronus",
+        name: "@styletransfer help",
         value: "Pull up this help menu."
       },
       {
@@ -48,7 +41,7 @@ exports.run = (client, config, message, args) => {
       timestamp: new Date(),
       footer: {
         icon_url: client.user.avatarURL,
-        text: "© Nile Wilson"
+        text: "© Zuoming Shi, Nile Wilson, Amy Yu"
       }
     }
 
