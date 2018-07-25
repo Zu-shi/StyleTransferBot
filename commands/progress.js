251d9d7dc85898e13805374365b6f7cbf2d8f657
Amy Yu <cm3yu@uwaterloo.ca>
2018-07-25 00:43:31 -0700

    progress command to show time remaining

---
+++ commands/progress.js
@@ -0,0 +1,27 @@
+exports.run = (client, config, message, args) => {
+  var fs = require("fs");
+  var contents = fs.readFileSync("/home/azureuser/neural-style/amy/progress.json", {"encoding": "utf-8"});
+  var data = JSON.parse(contents);
+  if (data.time_remaining == 0) {
+    message.channel.send("All done!");
+  }
+  else {
+    message.channel.send(hms(data.time_remaining) + " left :grin:");
+  }
+}
+
+function hms(seconds) {
+  seconds = seconds | 0; // truncate to int
+  hours = (seconds / (60 * 60)) | 0;
+  minutes = ((seconds / 60) % 60) | 0;
+  seconds = (seconds % 60) | 0;
+  if(hours > 0) {
+    return hours + ' hr ' + minutes + ' min';
+  }
+  else if (minutes > 0) {
+    return minutes + ' min ' + seconds + ' sec';
+  }
+  else {
+    return seconds + ' sec';
+  }
+}
