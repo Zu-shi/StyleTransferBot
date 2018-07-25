babec1a77dcdc035d0bc67bdcf0b58986f7b6158
Ubuntu <nilew@uw.edu>
2018-07-25 05:10:29 +0000

    changed from a to tmp_userid

--- run.bash
+++ run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python /home/azureuser/neural-style/neural-style/neural_style.py --content "/home/azureuser/StyleTransferBot/subject.jpg" --styles "/home/azureuser/StyleTransferBot/style_base.jpg" --output "results.jpg" --iterations 100 --print-iterations 10 --overwrite --maxwidth 500 --userid a --network /home/azureuser/neural-style/neural-style/imagenet-vgg-verydeep-19.mat
+python /home/azureuser/neural-style/neural-style/neural_style.py --content "/home/azureuser/StyleTransferBot/subject.jpg" --styles "/home/azureuser/StyleTransferBot/style_base.jpg" --output "results.jpg" --iterations 100 --print-iterations 10 --overwrite --maxwidth 500 --userid tmp_userid --network /home/azureuser/neural-style/neural-style/imagenet-vgg-verydeep-19.mat
